baffda338d08bd2fd288f74736201b0bcdc9a557
Joni92 <jonigonzalez2492@gmail.com>
2023-06-06 18:28:30 -0300

    First commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,99 @@
+const circleType = new CircleType(document.getElementById('rotated'));   
+
+      TweenMax.from(".artist", 2, {
+      delay: 5.4,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+      });
+
+      TweenMax.from(".watch", 2, {
+      delay: 5.5,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+      });
+
+      TweenMax.from(".menu", 2, {
+      delay: 5.6,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+      });
+
+      TweenMax.from(".rotatethis", 2, {
+      delay: 6,
+      opacity: 0,
+      ease: Expo.easeInOut
+      });
+
+      TweenMax.staggerFrom(".social-media ul li", 2, {
+      delay: 5.7,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+      }, 0.1);
+
+      TweenMax.staggerFrom(".listensong ul li", 2, {
+      delay: 5.9,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+      }, 0.1);  
+
+      TweenLite.fromTo(".hero-title h1", 0.3, {
+            x:-6,
+            y:2,
+            opacity: 0,
+      }, 
+      {
+            delay: 5,
+            x: 6,
+            y: -2,
+            opacity: 1,
+            ease:RoughEase.ease.config({
+                  strength:8,
+                  points:40, 
+                  template:Linear.easeNone, 
+                  randomize:false}),
+            clearProps:"all"
+      });
+
+      TweenLite.fromTo(".hero-title p", 0.3, {
+            x:-6,
+            y:2,
+            opacity: 0,
+      }, 
+      {
+            delay: 9,
+            x: 6,
+            y: -2,
+            opacity: 1,
+            ease:RoughEase.ease.config({
+                  strength:8,
+                  points:40, 
+                  template:Linear.easeNone, 
+                  randomize:false}),
+            clearProps:"all"
+      });
+
+
+// menu 
+
+let t1 = new TimelineMax({paused: true});
+
+t1.to('.menu-container', 0.4, {
+    right: '0%',
+});
+
+t1.staggerFrom('.link', 0.4, {opacity: 0}, 0.4, '-=0.2');
+
+t1.reverse();
+
+$(document).on('click', '.open-menu', function() {
+    t1.reversed(!t1.reversed())
+});
+
+$(document).on('click', '.close-menu', function() {
+    t1.reversed(!t1.reversed())
+});
